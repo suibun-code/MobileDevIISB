e5717860d3ca4ec745185a1e4a650097b81e68d1
Ramin Amiri <zeuspsn.55555@gmail.com>
2021-02-06 17:04:20 -0500

    Reverted back to original grid system

--- Assets/Scripts/Grid/3D Grid/Grid.cs
+++ Assets/Scripts/Grid/3D Grid/Grid.cs
@@ -11,9 +11,9 @@ public enum CellState
 public class Grid : MonoBehaviour
 {
     [SerializeField]
-    public static int CellSizeZ = 6;
+    public static int CellSizeZ = 32;
     [SerializeField]
-    public static int CellSizeX = 3;
+    public static int CellSizeX = 12;
 
     public GameObject cellPrefab;
     public Cell[,] allCells = new Cell[CellSizeX, CellSizeZ];
@@ -26,19 +26,19 @@ public class Grid : MonoBehaviour
                 GameObject newCell = Instantiate(cellPrefab, transform);
 
                 RectTransform rectTransform = newCell.GetComponent<RectTransform>();
-                rectTransform.position = new Vector3((j * 18) - 18f, rectTransform.position.y, (i * 24) - 60f);
+                rectTransform.position = new Vector3((j * 4) - 22f, rectTransform.position.y, (i * 4) - 62f);
 
                 allCells[j, i] = newCell.GetComponent<Cell>();
                 allCells[j, i].Setup(new Vector2Int(j, i), this);
             }
 
-        //for (int i = 0; i < CellSizeX; i += 2)
-        //    for (int j = 0; j < CellSizeZ; j++)
-        //    {
-        //        int offset = (j % 2 != 0) ? 0 : 1;
-        //        int finalX = i + offset;
+        for (int i = 0; i < CellSizeX; i += 2)
+            for (int j = 0; j < CellSizeZ; j++)
+            {
+                int offset = (j % 2 != 0) ? 0 : 1;
+                int finalX = i + offset;
 
-        //        allCells[finalX, j].GetComponent<SpriteRenderer>().color = new Color32(0, 0, 255, 30);
-        //    }
+                allCells[finalX, j].GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 30);
+            }
     }
 }
