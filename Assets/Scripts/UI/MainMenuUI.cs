b0a08effdd832d0c1f40e6dd45bac55676166d16
TeddyUm <43351089+TeddyUm@users.noreply.github.com>
2021-02-05 22:21:45 -0800

    option panels, minimap image add
    how to play, option, quit, load, credit button add (dummy image). Minimap image (dummy image)

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -5,8 +5,49 @@ using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
+    [SerializeField]
+    GameObject optionPanel;
+    [SerializeField]
+    GameObject HowToPlayPanel;
+    [SerializeField]
+    GameObject CreditPanel;
+
     public void StartButton()
     {
         GameManager.Instance.ChangeScene("Loading");
+    }
+
+    public void OptionWindowOpen()
+    {
+        optionPanel.SetActive(true);
+    }
+    public void OptionWindowClose()
+    {
+        optionPanel.SetActive(false);
+    }
+    public void HowToWindowOpen()
+    {
+        HowToPlayPanel.SetActive(true);
+    }
+    public void HowToWindowClose()
+    {
+        HowToPlayPanel.SetActive(false);
+    }
+    public void CreditWindowOpen()
+    {
+        CreditPanel.SetActive(true);
+    }
+    public void CreditWindowClose()
+    {
+        CreditPanel.SetActive(false);
+    }
+    public void LoadButton()
+    {
+        GameManager.Instance.ChangeScene("Loading");
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
     }
 }
