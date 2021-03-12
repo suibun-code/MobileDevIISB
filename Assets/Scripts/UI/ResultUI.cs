d611a00d41da5fc8146fffe6676eee0ddabd8149
Matthew Makepeace <Matthewmakepeace32@gmail.com>
2021-03-12 17:54:45 -0500

    Tried to Fix the Result Screen Buttons

--- Assets/Scripts/UI/ResultUI.cs
+++ Assets/Scripts/UI/ResultUI.cs
@@ -1,15 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ResultUI : MonoBehaviour
 {
+    public string SceneName;
+    public float time;
+
     public void RestartButton()
     {
-        GameManager.Instance.ChangeScene("MainGame");
+        SceneName = "tempPathScene";
+        StartCoroutine(LoadAsynSceneCoroutine());
     }
     public void MainmenuButton()
     {
-        GameManager.Instance.ChangeScene("MainMenu");
+        SceneName = "MainMenu";
+        StartCoroutine(LoadAsynSceneCoroutine());
+    }
+
+
+    IEnumerator LoadAsynSceneCoroutine()
+    {
+        // if loading already finish, wait until 5 second (too short).
+        AsyncOperation operation = SceneManager.LoadSceneAsync(SceneName);
+
+        operation.allowSceneActivation = false;
+
+
+        while (!operation.isDone)
+        {
+
+            time += Time.deltaTime;
+
+            //loadingBar.fillAmount = time / 3f;
+
+            if (time > 0.1)
+            {
+                operation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+
     }
 }
+
+
