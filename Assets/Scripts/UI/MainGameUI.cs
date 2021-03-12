ae92a33e4e4f9d0a98b5e7db3f89df4e6102cc3f
TeddyUm <43351089+TeddyUm@users.noreply.github.com>
2021-03-11 22:49:31 -0800

    Option add

--- Assets/Scripts/UI/MainGameUI.cs
+++ Assets/Scripts/UI/MainGameUI.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainGameUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject optionPanel;
 
-    // Update is called once per frame
-    void Update()
+    public void OptionOpen()
     {
-        
+        optionPanel.SetActive(true);
+        Time.timeScale = 0.0f;
+    }
+    public void OptionClose()
+    {
+        optionPanel.SetActive(false);
+        Time.timeScale = 1.0f;
     }
 }
