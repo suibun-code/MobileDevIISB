4f56468d81bb9c4aac22c8c1d254fa55d424dc8b
Ramin Amiri <zeuspsn.55555@gmail.com>
2021-03-15 00:14:53 -0400

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/AI/Enemies/MoveTo.cs
+++ Assets/Scripts/AI/Enemies/MoveTo.cs
@@ -5,13 +5,30 @@ using UnityEngine.AI;
 public class MoveTo : MonoBehaviour
 {
     public GameObject goal;
-    
+    private NavMeshAgent agent;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         goal = GameObject.Find ("Player");
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
         agent.destination = goal.transform.position;
     }
 
+    public IEnumerator Freeze(float secondsToFreeze)
+    {
+        Debug.Log("FREEZEEEEEEEE");
+
+        float speed = agent.speed;
+
+        agent.speed = 0;
+
+        yield return new WaitForSeconds(secondsToFreeze);
+
+        agent.speed = speed;
+    }
 }
